ea10b306b8f98c1c368b8d11dc7a8ac06d24e124
FREEWING-JP <freewing.jp@gmail.com>
2023-04-13 23:32:26 +0900

    add argv to input URL

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
 import os
+import sys
+
 # 切换工作目录到项目根目录
 project = os.path.split(os.path.realpath(__file__))[0]
 os.chdir(project)
@@ -14,6 +16,7 @@ def main():
     """
     # 测试用例
     url = 'http://2016.cctv.com/2016/08/22/VIDEdMJX5lDjx1mLeLBLQtf2160822.shtml'
+    url = sys.argv[1]
     get_download_link(url, quality_type=5, get_dlink_only=False, is_merge=True, is_remain=False)
 
 
